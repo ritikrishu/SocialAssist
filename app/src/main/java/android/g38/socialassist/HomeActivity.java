7e657c96f568fc0192aa6b586c291e2ad8ec92fc
Ritik Rishu <ritikrishu@gmail.com>
2016-04-23 09:39:19 +0530

    HomeActivity List memory consumption reduced.

--- app/src/main/java/android/g38/socialassist/HomeActivity.java
+++ app/src/main/java/android/g38/socialassist/HomeActivity.java
@@ -3,12 +3,9 @@ package android.g38.socialassist;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.g38.ritik.Database.ListAdapter1;
+import android.g38.ritik.AdaptersAndAnimators.HomeListAdapter;
 import android.g38.ritik.Database.SocialAssistDBHelper;
-import android.g38.sanyam.contentprovider.ForCp;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -17,21 +14,18 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     ProgressDialog dialog;
-    ListView listView;
+    RecyclerView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +33,7 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         dialog = new ProgressDialog(this);
-        listView = (ListView)findViewById(R.id.lvMain);
+        listView = (RecyclerView) findViewById(R.id.lvMain);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -67,19 +61,9 @@ public class HomeActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        listView.setLayoutManager(new LinearLayoutManager(HomeActivity.this));
 
-        listView.setRecyclerListener(new AbsListView.RecyclerListener() {
-            @Override
-            public void onMovedToScrapHeap(View view) {
-                view.destroyDrawingCache();
-            }
-        });
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            }
-        });
 
     }
 	
@@ -136,7 +120,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
-        listView.setAdapter(new ListAdapter1(HomeActivity.this,getLayoutInflater()));
+        listView.setAdapter(new HomeListAdapter(HomeActivity.this, getLayoutInflater()));
 
     }
 
